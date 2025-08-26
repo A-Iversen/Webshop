413b70abb2921c1f551f02a1f7200257846b151e
Mikkel Simon Ølting Jensen <msoj71393@edu.ucl.dk>
2025-08-26 12:25:28 +0200

    Made changes to DataBaseModel.mmd

---
+++ Webshop.Domain/Entities (Model)/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Webshop.Domain.Entities__Model_
+{
+    internal class Customer
+    {
+        int CustomerId { get; set; }
+        string Name { get; set; }
+        string Email { get; set; }
+        string Address { get; set; }
+
+        public Customer(int customerId, string name, string email, string address)
+        {
+            CustomerId = customerId;
+            Name = name;
+            Email = email;
+            Address = address;
+        }
+    }
+}
